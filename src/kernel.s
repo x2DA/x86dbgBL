763a9122e94436f4f9eb0fec3b1b5d2a4b82e64d
x2DA <turcanutiulian@gmail.com>
2025-11-16 14:50:12 +0200

    Add sys_wait routine, writeline preserve offset

--- src/kernel.s
+++ src/kernel.s
@@ -2,58 +2,71 @@ org 0x200
 VGATmem equ 0xb800 ; VGA text mode buffer start
 screendim equ 77fh ; 24*80-1, starting from the VGATmem
 
-xor ax, ax
+mov ax, 0x200
+mov sp, ax
+mov bp, sp
+
 xor di, di
 xor si, si
+xor ax, ax
 
 mov ds, ax
 mov es, ax
 mov ss, ax
+mov bx, ax
+mov cx, ax
+mov dx, ax
 
-mov ax, 0x200
-mov sp, ax
-mov bp, sp
-xor ax, ax
-
+call set_cursor
 jmp main
 
 ; Data goes here
 
 welcome db "Welcome to 2Sos!", 0
-
+cxtitle db "cx=", 0
 
 
 jmp main
 
 main:
-
+	; Clear screen
 	mov al, 20h ; ' '
 	mov ah, 07h
 	call fill_screen
 
+	; TODO: Evaluate
+	
+
+	; Show
 	mov si, welcome
 	mov ah, 07h
 	mov bx, 0h
 	call write_line
 
-	mov dh, 00h
-	mov dl, 10h
-	call set_cursor
+	; Testing in prod, ignore.
+	;mov si, cxtitle
+	;mov bx, 20h ; 1919-10 (for reg+val), dec width times id of reg, where ax=1, bx=2...
+	;call dump_cx
 
-	jmp force_shutdown
+	; Wait
+	push cx
+	mov cx, 200d
+	call ms2microseconds
+	call sys_wait
+	pop cx
 
+	; Loop
 jmp main
 
 
-
-;	SI (Zero terminated string),
-;	BX (Offset from top left)
+;	SI (Zero terminated string)
+;	BX (Offset from top left, gets set to offset of the last char)
 ;	AH (BG|FG)
 ; The offset shouldn't be a power of two, it is accounted for :)
 write_line:
 	push ax
-	push bx
 	push si
+	push es
 
 	call set_es_to_vidmem
 
@@ -69,7 +82,35 @@ write_line:
 	jmp .loop
 
 	.done:
+	shr bx, 1
+	pop es
 	pop si
+	pop ax
+ret
+
+
+; TODO
+;	SI (Zero terminated title)
+;	BX (Offset from top left)
+dump_cx:
+	push ax
+	push bx
+	push cx
+	push si
+	push es
+
+	call set_es_to_vidmem
+
+	mov ah, 07h
+	call write_line
+
+	mov ah, 1ch
+	mov al, 40h
+	call write_char
+
+	pop es
+	pop si
+	pop cx
 	pop bx
 	pop ax
 ret
@@ -100,18 +141,30 @@ fill_screen:
 ret
 
 
+; FIXME: Doesn't work, reads nothing
+;	ZF (Gets set if keystroke)
+;	AH (Gets set with BIOS scan code)
+;	AL (Gets set with ASCII character)
+; Non-blocking, overwrites AX, sets ZF
+get_key:
+	mov ah, 01h
+	int 16h
+ret
+
+
 ;	AX ( [(BG|FG)] [Char] )
 ; Where ( [AH] [AL] )
 ;	BX (Offset from top left)
 ; The offset shouldn't be a power of two, it is accounted for :)
 write_char:
 	push bx
-
+	push es
 	call set_es_to_vidmem
 
 	shl bx, 1
 
 	mov [es:bx], ax
+	pop es
 	pop bx
 ret
 
@@ -122,9 +175,9 @@ set_cursor:
 	push ax
 	push bx
 
-	mov ah, 0x02 ; Set cursor position
-	mov bh, 0 ; Page number
-	int 0x10
+	mov ah, 02h ; Set cursor position
+	mov bh, 0h ; Page number
+	int 10h
 
 	pop bx
 	pop ax
@@ -139,9 +192,31 @@ set_es_to_vidmem:
 ret
 
 
+;	CX (Expects time in ms)
+;	[CX:DX] (Gets set with the result in microseconds)
+ms2microseconds:
+	push ax
+
+	xchg ax, cx ; AX now holds the ms time
+	mov cx, 1000d ; 1ms = 1k microseconds
+	mul cx ; [DX:AX] now holds the result, move to [CX:DX]
+	
+	mov cx, dx
+	mov dx, ax
+	pop ax
+ret
+
+
+;	[CX:DX] (Time to wait in microseconds)
+sys_wait:
+	mov ah, 86h
+	int 15h
+ret
+
+
 
 jmp main
-force_shutdown:
+sys_force_shutdown:
 	; int 15, AX 5307h (APM state)
 	; BX (Device ID), CX (System State ID)
 
@@ -175,7 +250,7 @@ force_shutdown:
 		hlt
 	jmp halt_cpu
 
-jmp force_shutdown
+jmp sys_force_shutdown
 
 
 times 9216 - ($-$$) db 0 ; Pad out remaining 18 sectors
