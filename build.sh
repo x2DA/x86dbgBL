37dc971dc3fbc28390e3400245ef1092a5f670d4
x2DA <turcanutiulian@gmail.com>
2025-11-19 19:38:12 +0200

    Refactor: Simplify build scripts

--- build.sh
+++ build.sh
@@ -1,58 +1,7 @@
 #!/usr/bin/env bash
 SOURCEFOLDER="src/"
-CACHEFOLDER="cache/"
-BUILDFOLDER="./"
+TARGETSOURCEFILE="bootloader.s"
+OUTPUTIMAGE="bOS.img"
 
-OUTPUTIMAGE="os.img"
-
-# For intermediary files
-INFORMATEXT="s"
-OUTFORMATEXT="bin"
-
-
-# $1 filename
-buildsourcefile() {
-	if [[ -z "$1" ]]; then
-		echo "Parameter not specified."
-		exit
-	fi
-
-	if [ ! -f "${SOURCEFOLDER}$1" ]; then
-		echo "Error: ${SOURCEFOLDER}$1 does not exist."
-	fi
-	
-	BASENAME="${1%.*}"
-	nasm "${SOURCEFOLDER}$1" -f bin -o "${CACHEFOLDER}${BASENAME}.${OUTFORMATEXT}"
-} 
-
-# $1 base filename
-cachetoout() {
-	if [[ -z "$1" ]]; then
-		echo "Parameter not specified"
-		exit
-	fi
-
-	cat "${CACHEFOLDER}$1.${OUTFORMATEXT}" >> "${BUILDFOLDER}${OUTPUTIMAGE}"
-}
-
-# ----
-# main
-# ----
-
-if [[ ! -d ${SOURCEFOLDER} ]] || [[ ! -d ${CACHEFOLDER} ]] || [[ ! -d ${BUILDFOLDER} ]]; then
-	echo "Error: source, cache or build folder does not exist."
-	exit
-fi
-
-
-# Prepare output file
-touch "${OUTPUTIMAGE}"
-> "${OUTPUTIMAGE}"
-
-# Build output image
-buildsourcefile "bootloader.s"
-cachetoout "bootloader"
-
-buildsourcefile "kernel.s"
-cachetoout "kernel"
+nasm "${SOURCEFOLDER}${TARGETSOURCEFILE}" -f bin -o "${OUTPUTIMAGE}"
 
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-#qemu-system-i386 -drive file=os.img,format=raw,if=floppy
-qemu-system-i386 -drive file=os.img,format=raw
+qemu-system-i386 -drive file=bOS.img,format=raw -monitor stdio
 
